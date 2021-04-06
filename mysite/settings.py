28500fa63fab68728775360e91dc01f91f98fc26
Enrico Saccheggiani <34415973+gordongekko67@users.noreply.github.com>
2021-04-06 14:49:51 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ez(bnnh^uthqmbmu#(ab$j0b!%4p%&t4a_1t(w2v*^ogj32@%z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1',  'mysiteenrico.herokuapp.com']
 
